dc78d5480151b103a792f08ef3ec37d88d29d81d
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-05 22:21:49 +0530

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1>hello</h1>
+      <h2>hello</h2>hello</div>
+
+      
+    </>
+  )
+}
+
+export default App
