1bdfe78129103163e912b7c239e050ce4ab0a232
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-08 23:43:44 +0530

    add cards

--- cards-project/props/src/App.jsx
+++ cards-project/props/src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Bookmark, Save } from 'lucide-react'
 
 const App = () => {
   return (
-    <div className='parent'>hello</div>
+    <div className='parent'>
+      <div className='card'>
+        <div className='top'>
+          <img src="https://imgs.search.brave.com/uU9b7LBA01u46O4i-Xh3WyNsbaVVD4mmVNMUF0dhp3g/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvNTAwcC8w/MS8xMC9hbWF6b24t/bG9nby1ibGFjay1h/bmQteWVsbG93LXZl/Y3Rvci00Njg2MDEx/MC5qcGc" alt="" />
+          <button>
+            Save <Bookmark />
+          </button>
+          </div> 
+          <div className='center'>
+            <h3>Amazone <span> 5 days ago</span></h3>
+            <h2>Senior UI/UX Designer </h2>
+            <div>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+          <div className="bottom">
+            <div>
+              <div>
+                <h3>$130/h</h3>
+                <p>Mumbai, India</p>
+              </div>
+              <button>Apply Now</button>
+            </div>
+
+          </div>
+          </div>
+    </div>
   )
 }
 
