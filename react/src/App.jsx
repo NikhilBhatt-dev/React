a510642337c58438be1d8c3b2dc7006a6ab199dc
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-08 19:24:29 +0530

    Added new props project inside cards-project and updated App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
    
-    <Card user='aman'   age="18" img="https://plus.unsplash.com/premium_photo-1723526592766-15d1fd0de4a5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332"/>
+    <Card user="aman"   age="18" img="https://plus.unsplash.com/premium_photo-1723526592766-15d1fd0de4a5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332"/>
     <Card user='nikhil' age="23" img="https://media.istockphoto.com/id/2152349760/photo/human-red-skull-on-black-background.jpg?s=1024x1024&w=is&k=20&c=RKidfSF95s-qut4TAWP2ghgJ4-z-mljlox7GGmI5w7U=" />
 
     </>
