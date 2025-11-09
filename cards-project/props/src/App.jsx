bb039c1bc332acaba86d74ab649533bf69ad6c49
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-09 11:25:04 +0530

    adding arr object

--- cards-project/props/src/App.jsx
+++ cards-project/props/src/App.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import Card from "./components/Card"
+import User from './components/User'
+
 
 const App = () => {
+
+  const arr =[
+    {
+      user: "honey",
+      age:50
+    },
+    {
+      user: "arjit",
+      age:43
+    },
+    {
+      user: "jubin",
+      age:47
+    },
+    {
+      user: "atif",
+      age:49
+    },
+
+  ]
+
+
   return (
     <div className='parent'> 
-        
-      <Card />
-      <Card />
 
-          
+     {arr.map(function(elem){
+      console.log(elem.age);
+      
+     })} 
+
+
+       
+      {/* <User name={arr[0]} /> */}
+      <User name="nikhil1" />
+      <User name="nikhil2" />
           
 
     </div>
