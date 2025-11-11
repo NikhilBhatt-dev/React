e40797940b15703902cc9cf19de2d3787d5a1125
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-11 22:50:20 +0530

    complete-navbar

--- Ui/vite-project/src/App.jsx
+++ Ui/vite-project/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Section1 from './components/Section1/Section1'
 import Section2 from './components/Section2/Section2'
+import Navbar from './components/Section1/Navbar'
 const App = () => {
   return (<>
-    
-    <Section1 />
-    <Section2 />
+
+  
+     <Navbar />
 
     </>
   )
---
+++ Ui/vite-project/src/components/Section1/Page1Contest.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page1Contest = () => {
+  return (
+    <div>Page1Contest</div>
+  )
+}
+
+export default Page1Contest
